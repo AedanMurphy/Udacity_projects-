50b2c2d00306db6d7d73a0680bae7929a691dce1
AedanMurphy <84075464+AedanMurphy@users.noreply.github.com>
2021-10-09 23:10:23 +1100

    Update polarityScore.test.js

--- evaluate-news-nlp-p4/___test___/polarityScore.test.js
+++ evaluate-news-nlp-p4/___test___/polarityScore.test.js
@@ -2,15 +2,21 @@ import { polarityScore } from "../src/client/js/formHandler";
 
 describe('Testing the polarity score functionality', () => {
     test('Testing polarityScore()', () => {
-            expect(polarityScore('N')).toBe('neutral')
+        expect(polarityScore('P+')).toBe('Strongly Positive')
     })
     test('Testing polarityScore()', () => {
-        expect(polarityScore('P+')).toBe('Strongly positive')
+        expect(polarityScore('P')).toBe('Positive')
+    })
+    test('Testing polarityScore()', () => {
+            expect(polarityScore('N')).toBe('Neutral')
     })
     test('Testing polarityScore()', () => {
         expect(polarityScore('P-')).toBe('Negative')
     })
     test('Testing polarityScore()', () => {
-        expect(polarityScore('None')).toBe('None')
+        expect(polarityScore('P--')).toBe('Strongly Negative')
+    })
+    test('Testing polarityScore()', () => {
+        expect(polarityScore('N/A')).toBe('None')
     })
 });
