f5858a3ff48735d3af83d8e7770284a8cccf751e
AedanMurphy <84075464+AedanMurphy@users.noreply.github.com>
2021-10-22 07:37:04 +1100

    Add files via upload

---
+++ FEND Capstone - Travel App/src/Client/index.js
@@ -0,0 +1,16 @@
+
+import '../Client/styles/style.scss';
+
+//follow these js imports as a guide to function
+import {genCoordinates} from './js/app'
+import {getWeatherBitAPI} from './js/app'
+import {postAllData} from './js/app'
+
+/*import js
+import scss 
+ export js
+ export scss*/
+
+ alert('I exist!')
+
+ export {genCoordinates, getWeatherBitAPI, postAllData};
