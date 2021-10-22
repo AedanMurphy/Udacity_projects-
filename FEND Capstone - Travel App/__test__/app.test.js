3a0ccf075e256efed0beb62070767acdbd1a0d57
AedanMurphy <84075464+AedanMurphy@users.noreply.github.com>
2021-10-23 07:25:28 +1100

    Add files via upload

---
+++ FEND Capstone - Travel App/__test__/app.test.js
@@ -0,0 +1,7 @@
+import getCount from '../src/Client/js/app'
+
+describe('Testing funcionality', () => {
+    it('should the getCount() function test that be true === true', () => {
+      expect(getCount).toBe(getCount)
+    })
+  })
