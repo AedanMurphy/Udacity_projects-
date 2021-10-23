e2c6d9a4c844883f8ae94616ddaafbf351dca314
AedanMurphy <84075464+AedanMurphy@users.noreply.github.com>
2021-10-24 07:22:23 +1100

    Delete FEND Capstone - Travel App directory

--- FEND Capstone - Travel App/__test__/app.test.js
+++
@@ -1,7 +0,0 @@
-import getCount from '../src/Client/js/app'
-
-describe('Testing funcionality', () => {
-    it('should the getCount() function test that be true === true', () => {
-      expect(getCount).toBe(getCount)
-    })
-  })
